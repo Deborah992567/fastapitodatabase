3d542fc1e34481eff86c8a11d2346ab8b59e08b4
uchechiiwuh <debbieofficial214@gmail.com>
2025-07-21 15:57:30 +0200

    added the method get_db for the database in main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from multiprocessing.resource_tracker import getfd
 from fastapi import Fastapi , HTTPException, Depends
 from pydantic import BaseModel
 from typing import List , Annotated
+import models
+from database import engine , SessionLocal
+from sqlalchemy.orm import Session
+models.Base.metadata.create_all(bind=engine) 
 
 app = Fastapi()
 
@@ -11,3 +16,12 @@ class ChoiceBase(BaseModel):
 class QuestionBase(BaseModel):
     question_text : str
     choices : List[ChoiceBase]
+    
+def get_db():
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+            
+db_dependency = Annotated[Session,Depends(get_db)]            
